e6a4dcd5c9b774b892b7a03a78776c469b4bd2ae
ilma-s <ilma2.spahic@gmail.com>
2023-11-03 11:50:51 +0100

    Address comments

--- frontend/src/components/appPath/AppPath.tsx
+++ frontend/src/components/appPath/AppPath.tsx
@@ -14,22 +14,23 @@ const AppPath = () => {
     const remainingSegments = pathnameSegments.slice(2);
 
     const pathElements = remainingSegments.map((segment, index) => (
-        <div key={index}>
-            <p className="font-bold text-trueIndigo-500">
-                {capitalizeUrlFragments(mappedPaths[segment] || segment)}
-            </p>
+        <div key={index} className="flex gap-4 font-lato text-trueGray-800">
+            <img src={arrowIcon} alt="Arrow Icon" className="pr-2" />
+            <Link to={segment}>
+                <p className="font-bold text-trueIndigo-500">
+                    {capitalizeUrlFragments(mappedPaths[segment] || segment)}
+                </p>
+            </Link>
         </div>
     ));
 
     return (
         <div className="flex gap-4 font-lato text-trueGray-800">
             <Link to={firstSegmentPath}>
-                {' '}
                 <p className="font-light pr-2">
                     {capitalizeUrlFragments(firstSegmentPath)}
                 </p>
             </Link>
-            <img src={arrowIcon} alt="Arrow Icon" className="pr-2" />
             {pathElements}
         </div>
     );
