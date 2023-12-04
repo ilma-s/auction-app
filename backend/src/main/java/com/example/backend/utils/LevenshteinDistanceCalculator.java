2f7cad165d69421b4a487adae19fce47560a82e2
ilma-s <104169760+ilma-s@users.noreply.github.com>
2023-12-04 19:53:37 +0100

    Add did you mean (#9)
    * Initial single product page
    
    * Fix bugs
    
    * Update time conversion function
    
    * Address comments
    
    * Initial commit
    
    * Initial commit
    
    * Initial version
    
    * Initial shop page version
    
    * Create loading spinner
    
    * Filter by categories from homepage
    
    * Add infinite scroll
    
    * Enhance search capabilities
    
    * Alter infinite scroll logic
    
    * Redefine subcategory selection
    
    * Address comments
    
    * Address comments v1
    
    * Cleanup and refactor backend
    
    * Address comments
    
    * Add initial autocorrect logic
    
    * Cleanup
    
    * Refine state management
    
    * Refactor + Cleanup
    
    * Enhance search capabilities
    
    * address comments
    
    * resolve comments

---
+++ backend/src/main/java/com/example/backend/utils/LevenshteinDistanceCalculator.java
@@ -0,0 +1,36 @@
+package com.example.backend.utils;
+
+import java.util.Arrays;
+
+public class LevenshteinDistanceCalculator {
+    public static int calculate(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        for (int i = 0; i <= word1.length(); i++) {
+            for (int j = 0; j <= word2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = min(
+                            dp[i - 1][j - 1] + costOfSubstitution(word1.charAt(i - 1), word2.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1
+                    );
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+
+    private static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    private static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
+    }
+}
